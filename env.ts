7b01166540d9197264efa9888703e980666cd8bb
MrAnri29 <mr.anri2007@gmail.com>
2024-08-23 12:59:35 +0400

    Initial commit

---
+++ env.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "test", "production"])
+    .default("development"),
+  DISCORD_TOKEN: z.string(),
+  DISCORD_CLIENT_ID: z.string(),
+});
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  console.error("❌ Invalid environment variables:", parsedEnv.error.format());
+  process.exit(1);
+}
+
+export const env = parsedEnv.data;
